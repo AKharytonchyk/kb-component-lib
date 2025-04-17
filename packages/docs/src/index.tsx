5d1252c8e80a80599e7b1cea114fc8eeb0d19115
Artsiom Kharytonchyk <artsiom.kharytonchyk@coxautoinc.com>
2025-04-17 15:21:36 +0200

    feat: Make a release and deployment

--- packages/docs/src/index.tsx
+++ packages/docs/src/index.tsx
@@ -4,7 +4,10 @@ import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 import './index.scss';
 
-const router = createRouter({ routeTree });
+const router = createRouter({ 
+  routeTree,
+  basepath: (import.meta as any).env.MODE === 'production' ? '/kb-component-lib' : '/',
+ });
 
 declare module '@tanstack/react-router' {
   interface Register {
